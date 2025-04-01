d2cce32b743cbb0610df8053e736bfe6c7c887cb
AlexLomonosove <106763521+AlexLomonosove@users.noreply.github.com>
2025-04-01 16:51:49 +0300

    first commit

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from product.models import Category,Product,Transaction,ProductPhoto
+from django.utils.translation import gettext_lazy as _
 from mptt.admin import MPTTModelAdmin,TreeRelatedFieldListFilter
+from django.shortcuts import redirect
+from urllib.parse import urlencode
+
 # Register your models here.
 
 admin.site.site_header = 'Администрация TechnoKB'
@@ -23,13 +27,34 @@ class CategoryAdmin(MPTTModelAdmin):
     list_filter = (('parent', admin.RelatedFieldListFilter),)
     search_fields = ('name',)
 
+class InStockFilter(admin.SimpleListFilter):
+    title = _('В наличии')  
+    parameter_name = 'in_stock'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('yes', _('Да')),
+            ('no', _('Нет')),
+        ]
+    
+    def choices(self, changelist):
+        choices = super().choices(changelist)
+        return [choice for choice in choices if choice['display'] != _('All')]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'yes':
+            return queryset.filter(in_stock="Да")
+        if self.value() == 'no':
+            return queryset.filter(in_stock="Нет")
+        return queryset
+
 
 class ProductAdmin(admin.ModelAdmin):
     
     inlines = [AdminPhoto,TransactionAdmin]    
     list_display = ('name','category','count','in_stock','is_published','created_at','amount_of_transaction',)
     search_fields = ('name',)
-    list_filter = (('category',TreeRelatedFieldListFilter),'in_stock',)    
+    list_filter = (('category',TreeRelatedFieldListFilter),InStockFilter,)   
     ordering = ('name',)
     readonly_fields = ('count','average_price','total_count','in_stock','created_at','amount_of_transaction',)
     autocomplete_fields = ('category',) 
@@ -37,7 +62,11 @@ class ProductAdmin(admin.ModelAdmin):
     class Meta:
         model = Product
        
-
+    def changelist_view(self, request, extra_context=None):        
+        if 'in_stock' not in request.GET:
+            query_string = urlencode({'in_stock': 'yes'})
+            return redirect(f"{request.path}?{query_string}")
+        return super().changelist_view(request, extra_context)
 
 
 admin.site.register(Category,CategoryAdmin)
--- product/models.py
+++ product/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save,post_delete
 from django.dispatch import receiver
 from order.models import Order
 from mptt.models import MPTTModel, TreeForeignKey
@@ -30,7 +30,7 @@ class Product(models.Model):
     created_at = models.DateTimeField("Дата",auto_now_add=True)
     amount_of_transaction = models.PositiveSmallIntegerField("Количество транзакций", default=0)
     is_published = models.CharField("Опубликовано", default="Нет", max_length=3, choices=(("Да","Да"),("Нет","Нет")))
-    
+   
     def __str__(self):
         return f"{self.name}"
     
@@ -67,10 +67,9 @@ class Transaction(models.Model):
         verbose_name = "транзакция"
         verbose_name_plural = "Транзакции"
 
-@receiver(post_save, sender=Transaction)
-def update_product_total_count(sender, instance, **kwargs):    
+
+def update_product_count(instance):
     product = instance.product
-    
     #Пересчёт общего количества:  
     transactions = Transaction.objects.filter(product = product)
     
@@ -99,3 +98,13 @@ def update_product_total_count(sender, instance, **kwargs):
     product.save() 
     
     
+
+
+@receiver(post_save, sender=Transaction)
+def update_product_total_count(sender, instance, **kwargs):    
+    update_product_count(instance)
+    
+@receiver(post_delete, sender=Transaction)
+def update_product_total_count_on_delete(sender, instance, **kwargs):
+    update_product_count(instance)
+
